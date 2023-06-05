012e61e8760ec29ecb5e1bef485f2899b0372266
Zumbua <flpyounan@yahoo.com.br>
2023-06-05 15:06:56 -0300

    Rock, paper and Scisors

--- Rock - paper - scissors.py
+++ Rock - paper - scissors.py
@@ -1,9 +1,14 @@
 import random
+w1 = 0
+w2 = 0
+
 
 while True:
     choices = ["rock", "paper", "scissors"]
     computer = random.choice(choices)
     player = None
+
+
 
     while player not in choices:
         player = input("choose rock, paper or scissors: ").lower()
@@ -13,10 +18,19 @@ while True:
 
     if player == computer:
             print("we have a draw!")
-    elif (player =="rock" and computer=="scossor") or (player == "scissor" and computer =="paper") or (player== "paper" and computer=="rock"):
-            print("You Win")
+    elif (player =="rock" and computer=="scissors") or (player == "scissors" and computer =="paper") or (player== "paper" and computer=="rock"):
+            w1=w1+1
+            print("You Win!")
     else:
+            w2=w2+1
             print("You Lose!")
-    play_again = input("Do you wish to play again? (Y/N): ").lower()
-    if play_again != "y":
+
+
+    print(f"Score is Player/Computer:", w1, "/", w2 )
+
+
+    play_again = input("Do you wish to play again? (Yes/No): ").lower()
+    if play_again != "yes":
         break
+
+print(f"Final score(Player - Computer):",w1,w2)
